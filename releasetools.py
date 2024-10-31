209ed927e6234c5f2d6a329562bd6e465e4384b9
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-10-31 11:10:09 +0600

    Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
-# Copyright (c) 2011, The Linux Foundation. All rights reserved.
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2019 The Mokee Open Source Project
+# Copyright (C) 2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,20 +17,15 @@
 import common
 import re
 
+def AddImage(info, dir, basename, dest):
+  name = basename
+  data = info.input_zip.read(dir + "/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 def FullOTA_InstallBegin(info):
   AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
-  flash_script = open("device/xiaomi/whyred/partitions/flash_super_dummy.sh", 'r').read()
-  common.ZipWriteStr(info.output_zip, "install/bin/flash_super_dummy.sh", flash_script);
   info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
-  info.script.AppendExtra('run_program("/sbin/sh", "/tmp/flash_super_dummy.sh");')
+  info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
+  info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
   return
-
-def AddImage(info, dir, basename, dest):
-  path = dir + "/" + basename
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
